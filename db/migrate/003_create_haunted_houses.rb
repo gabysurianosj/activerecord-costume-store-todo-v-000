7a502a93b909b21a8e8c08a990b95c7305322937
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-02 14:17:30 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name
+      t.integer :price 
       t.string :location
       t.string :theme
       t.boolean :family_friendly
